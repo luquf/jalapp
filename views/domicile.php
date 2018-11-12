4ba76340a0e08c5d6e30fcd4e43df7fc728a9acb
Augustin du Besset <augustin.dubesset@sfr.fr>
2018-11-12 18:46:17 +0100

    popup ajout domicile onglet

--- views/domicile.php
+++ views/domicile.php
@@ -57,15 +57,43 @@
 					});
 				});
 			});
-		</script>
+        </script>
+        
 	<div class= 'tabs'>
 		<ul class='tabs'>
 			<li><a href='#tab1'>Domicile 1</a></li>
 			<li><a href='#tab2'>Domicile 2</a></li>
             <li><a href='#tab3'>Domicile 3</a></li>
-            <li><input class='button' type="button"  value='+' onclick="javascript:location.href='inscription.php'"/></li>
-		</ul>
-		<div id='tab1'>
+            <li><input class='button' type="button"  value='+' onclick="openModal()"/></li>
+        </ul>
+
+
+        <div id = "modal">
+  			 <h1> Ajout Domicile</h1>
+			   <div class="infos"> 
+                </br>
+                        <p>
+                            <input type="type" name="type" id="type" placeholder="Type de domicile : " size="30" maxlength="20"/>
+                            <input type="submit" value="Valider"/>
+                        </p>
+                        <p>
+                            <input type="rooms" name="rooms" id="rooms" placeholder="Nombre de pièces : " size="30" maxlength="20"/>
+                            <input type="submit" value="Valider"/>
+                        </p>
+			
+                        <p>
+                            <input type="capteurs" name="capteurs" id="capteurs" placeholder="Nombre de capteurs" size="30" maxlength="30" />
+                            <input type="submit" value="Valider"/>
+                        </p>
+                        
+        </div>
+
+		<button id ="close" onclick ="closeModal()">X</button>
+		<button id ="validation" onclick ="closeModal()">Valider</button>
+    	</div>
+    	<script src="app.js" type="text/javascript"></script>
+        
+        <div id='tab1'>
 			<h3>Liste des Pièces</h3>
 			<p>
             <a href= "piece1.php" style= "background: #3A2D8C; color: white; text-decoration : underline;">Pièce 1</a>			
