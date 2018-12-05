d02d522c146e2f767440c19acdf61414df62312c
ArthurDemuijnck <arthurdemuijnck@gmail.com>
2018-12-05 17:31:06 +0100

    ACCUEIL VRAIMENT FINI

--- views/accueil.php
+++ views/accueil.php
@@ -9,13 +9,13 @@
 	<body>
 	<div id="bandeau">
 			<div class="logo">
-				<p>
+
 					<a href="accueil.php" style="text-decoration:none"> <img src="../public/assets/logo.png" alt = "Logo Domisep" id="logo" title = "Logo Domisep" /> 
-				</p>
+
 			</div>
 
-			<div class="connexion">
-				<p>
+			<div class="bandeau-droite">
+	
 
 				<!-- <select name="Langue" id="Langue" >
 					<option value="Français">Français</option>
@@ -24,7 +24,7 @@
 				<a class="connexion" href="inscription.php"> Connexion</a>	
 				<a class= "aide" href= "aide_accueil.php" >Aide </a>
 
-				</p>
+		
 			</div>
 		</div>
 
@@ -37,9 +37,9 @@
 
 		
 		<div id="apropos">
-			<h2>
+			<h3>
 				A propos
-			</h2>
+			</h3>
 				<p>
 					Unde Rufinus ea tempestate praefectus praetorio ad discrimen trusus est ultimum. ire enim ipse compellebatur ad militem, quem exagitabat inopia simul et feritas, et alioqui coalito more in ordinarias dignitates asperum semper et saevum, ut satisfaceret atque monstraret, quam ob causam annonae convectio sit impedita.
 Haec igitur Epicuri non probo, inquam. De cetero vellem equidem aut ipse doctrinis fuisset instructior est enim, quod tibi ita videri necesse est, non satis politus iis artibus, quas qui tenent, eruditi appellantur aut ne deterruisset alios a studiis. quamquam te quidem video minime esse deterritum.
