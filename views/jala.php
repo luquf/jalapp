1f805f470ea0417900af5c6e8232217fbf10206c
JamesGranie <jamesgranie@gmail.com>
2018-12-13 16:52:21 +0100

    page JALA

--- views/jala.php
+++ views/jala.php
@@ -17,20 +17,32 @@
 
 		<div id="slogan">
 			<h1>
-				Domisep : La domotique en un clic !
+				JALA
 			</h1>
+
+			<h2>
+				Crafting tommorows softwares.
+			</h2>
+
 		</div>
 
 		
-		<div id="apropos">
-			<h3>
-				A propos
-			</h3>
-				<p>
-					Unde Rufinus ea tempestate praefectus praetorio ad discrimen trusus est ultimum. ire enim ipse compellebatur ad militem, quem exagitabat inopia simul et feritas, et alioqui coalito more in ordinarias dignitates asperum semper et saevum, ut satisfaceret atque monstraret, quam ob causam annonae convectio sit impedita.
-Haec igitur Epicuri non probo, inquam. De cetero vellem equidem aut ipse doctrinis fuisset instructior est enim, quod tibi ita videri necesse est, non satis politus iis artibus, quas qui tenent, eruditi appellantur aut ne deterruisset alios a studiis. quamquam te quidem video minime esse deterritum.
-Novitates autem si spem adferunt, ut tamquam in herbis non fallacibus fructus appareat, non sunt illae quidem repudiandae, vetustas tamen suo loco conservanda; maxima est enim vis vetustatis et consuetudinis. Quin in ipso equo, cuius modo feci mentionem, si nulla res impediat, nemo est, quin eo, quo consuevit, libentius utatur quam intractato et novo. Nec vero in hoc quod est animal, sed in iis etiam quae sunt inanima, consuetudo valet, cum locis ipsis delectemur, montuosis etiam et silvestribus, in quibus diutius commorati sumus.
-				</p>
+		<div id="team">
+
+			<div class="equipe">
+				<h3>
+				L'ÉQUIPE
+				</h3>
+			</div>
+
+			<div class="photos_team">
+				<img src="../public/assets/pp_leo.jpg" id="photo_profil"/>
+			<div>
+
+
+
+
+				
 		</div>
 </body>
 </html>
