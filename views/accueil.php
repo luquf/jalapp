279612b75f7390fe44f21ea67e8d9721b3ff688e
JamesGranie <jamesgranie@gmail.com>
2018-12-18 12:00:21 +0100

    centrer a propos + début footer

--- views/accueil.php
+++ views/accueil.php
@@ -8,22 +8,22 @@
 	</head>
 
 	<body>
-	<div id="bandeau">
-			<div class="logo">
-					<a href="domicile.php" style="text-decoration:none"> <img src="../public/assets/logo.png" alt = "Logo Domisep" id="logo" title = "Logo Domisep" /> 
+		<div id="bandeau">
+				<div class="logo">
+						<a href="domicile.php" style="text-decoration:none"> <img src="../public/assets/logo.png" alt = "Logo Domisep" id="logo" title = "Logo Domisep" /> 
 
-			</div>
-
-			<div id="bandeau_droite">
-					<div class="aide">
-						<a href= "aide_accueil.php" style = "text-decoration: none; color: #515659"	> Aide </a>
-					</div>
-				<div class="connexion">
-					<a href="inscription.php" style = "text-decoration: none; color: #515659"> Connexion </a>
 				</div>
-			</div>
 
-		
+				<div id="bandeau_droite">
+						<div class="aide">
+							<a href= "aide_accueil.php" style = "text-decoration: none; color: #515659"	> Aide </a>
+						</div>
+					<div class="connexion">
+						<a href="inscription.php" style = "text-decoration: none; color: #515659"> Connexion </a>
+					</div>
+				</div>
+
+			
 		</div>
 
 
@@ -33,16 +33,28 @@
 			</h1>
 		</div>
 
-		
+			
 		<div id="apropos">
-			<h3>
+			<h3 class = "titre_apropos">
 				A propos
 			</h3>
 				<p>
 					Unde Rufinus ea tempestate praefectus praetorio ad discrimen trusus est ultimum. ire enim ipse compellebatur ad militem, quem exagitabat inopia simul et feritas, et alioqui coalito more in ordinarias dignitates asperum semper et saevum, ut satisfaceret atque monstraret, quam ob causam annonae convectio sit impedita.
-Haec igitur Epicuri non probo, inquam. De cetero vellem equidem aut ipse doctrinis fuisset instructior est enim, quod tibi ita videri necesse est, non satis politus iis artibus, quas qui tenent, eruditi appellantur aut ne deterruisset alios a studiis. quamquam te quidem video minime esse deterritum.
-Novitates autem si spem adferunt, ut tamquam in herbis non fallacibus fructus appareat, non sunt illae quidem repudiandae, vetustas tamen suo loco conservanda; maxima est enim vis vetustatis et consuetudinis. Quin in ipso equo, cuius modo feci mentionem, si nulla res impediat, nemo est, quin eo, quo consuevit, libentius utatur quam intractato et novo. Nec vero in hoc quod est animal, sed in iis etiam quae sunt inanima, consuetudo valet, cum locis ipsis delectemur, montuosis etiam et silvestribus, in quibus diutius commorati sumus.
+	Haec igitur Epicuri non probo, inquam. De cetero vellem equidem aut ipse doctrinis fuisset instructior est enim, quod tibi ita videri necesse est, non satis politus iis artibus, quas qui tenent, eruditi appellantur aut ne deterruisset alios a studiis. quamquam te quidem video minime esse deterritum.
+	Novitates autem si spem adferunt, ut tamquam in herbis non fallacibus fructus appareat, non sunt illae quidem repudiandae, vetustas tamen suo loco conservanda; maxima est enim vis vetustatis et consuetudinis. Quin in ipso equo, cuius modo feci mentionem, si nulla res impediat, nemo est, quin eo, quo consuevit, libentius utatur quam intractato et novo. Nec vero in hoc quod est animal, sed in iis etiam quae sunt inanima, consuetudo valet, cum locis ipsis delectemur, montuosis etiam et silvestribus, in quibus diutius commorati sumus.
 				</p>
 		</div>
-</body>
+		
+	</body>
+
+	<footer id="footer">
+
+		<div class="designed"> Powered by </div>
+		<div class="logo_JALA"> 
+			<a href = "jala.php"> <img id="logo_JALA" src="../public/assets/logo_JALA.png" alt = "Logo JALA" title = "Logo JALA" width="70" height="34" /></a>
+		</div>
+		<div class="designed"> © </div>
+
+	</footer>
+	
 </html>
