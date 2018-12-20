0acf8db20033a37581b4ae8c9839e62adac949df
JamesGranie <jamesgranie@gmail.com>
2018-12-20 17:30:33 +0100

    page JALA terminado

--- views/jala.php
+++ views/jala.php
@@ -3,25 +3,21 @@
 	<head>
 		<meta charset="utf-8"/>
 		<link rel="stylesheet" href="../public/css/jala.css" />
-		<link rel="icon" type="image/png" href="../public/assets/logo_jala1.png" />
-		<title>JALA : Accueil</title>
+		<link rel="icon" type="image/png" href="../public/assets/logo_jala.png" />
+		<title>JALA</title>
 	</head>
 
 	<body>
 
-		<div>
+		<div id="bandeau">
 			<img src="../public/assets/logo_jala1.png" class="logo"/> 
 		</div>
-
-
 
 		<div id="slogan">
 			<h1>
 				JALA
-			</h1>
-
-			<h2>
-				Crafting tommorows softwares.
+<br>
+				<i>Crafting tommorows softwares.</i>
 			</h2>
 
 		</div>
@@ -31,18 +27,19 @@
 
 			<div class="equipe">
 				<h3>
-				L'ÉQUIPE
+				L'ÉQUIPE : 
 				</h3>
 			</div>
 
 			<div class="photos_team">
+				<img src="../public/assets/pp_arthur.jpg" id="photo_profil"/>
 				<img src="../public/assets/pp_leo.jpg" id="photo_profil"/>
-			<div>
+				<img src="../public/assets/pp_augustin.jpg" id="photo_profil"/>
+				<img src="../public/assets/pp_rames.jpg" id="photo_profil"/>
 
-
-
-
-				
+			<div>			
 		</div>
-</body>
+
+	</body>
+
 </html>
