24c7f361f7232e8b668c4c36b1f8f321b94396f5
Leo Berton <leoantoineberton@gmail.com>
2019-01-17 19:54:45 +0100

    mentions legales responsives

--- views/infocapteur.php
+++ views/infocapteur.php
@@ -21,7 +21,6 @@ if (isset($_GET['capteur'])) {
 <html>
 	<head>
 		<meta charset="utf-8"/>
-		<script src="../public/js/addonglet.js" type="text/javascript"></script>
 		<link rel="stylesheet" href="../public/css/piece1.css" />
 		<link rel="icon" type="image/png" href="../public/assets/favicon.png" />
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
