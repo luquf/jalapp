491b40529d8ec1e8cf04f89a938a55049cbf3877
ArthurDemuijnck <arthurdemuijnck@gmail.com>
2019-01-17 16:00:04 +0100

    accueil texte

--- views/accueil.php
+++ views/accueil.php
@@ -45,10 +45,15 @@
 			<h3 class = "titre_apropos">
 				A propos
 			</h3>
-				<p>
-					Unde Rufinus ea tempestate praefectus praetorio ad discrimen trusus est ultimum. ire enim ipse compellebatur ad militem, quem exagitabat inopia simul et feritas, et alioqui coalito more in ordinarias dignitates asperum semper et saevum, ut satisfaceret atque monstraret, quam ob causam annonae convectio sit impedita.
-	Haec igitur Epicuri non probo, inquam. De cetero vellem equidem aut ipse doctrinis fuisset instructior est enim, quod tibi ita videri necesse est, non satis politus iis artibus, quas qui tenent, eruditi appellantur aut ne deterruisset alios a studiis. quamquam te quidem video minime esse deterritum.
-	Novitates autem si spem adferunt, ut tamquam in herbis non fallacibus fructus appareat, non sunt illae quidem repudiandae, vetustas tamen suo loco conservanda; maxima est enim vis vetustatis et consuetudinis. Quin in ipso equo, cuius modo feci mentionem, si nulla res impediat, nemo est, quin eo, quo consuevit, libentius utatur quam intractato et novo. Nec vero in hoc quod est animal, sed in iis etiam quae sunt inanima, consuetudo valet, cum locis ipsis delectemur, montuosis etiam et silvestribus, in quibus diutius commorati sumus.
+			<h4>Bienvenue chez Domisep, un service de domotique personnel ! 
+				<p class = "textdomisep">
+
+					- Contrôler vos lampes, chauffages, volets à distance depuis votre ordinateur ou votre smartphone</br>
+					- 6 différents controlleurs/capteurs pour toutes vos pièces</br>
+					- Gérez une ou plusieurs maison </br>
+					- Accès facile à vos informations de consommation</br>
+</br>
+
 				</p>
 		</div>
 		
