951eaefbefef474b15e9583366592b9dea282e3d
Leo Berton <leoantoineberton@gmail.com>
2019-01-23 16:54:13 +0100

    db edit

--- database/database.sql
+++ database/database.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le :  jeu. 20 déc. 2018 à 13:21
--- Version du serveur :  10.1.37-MariaDB
--- Version de PHP :  7.3.0
+-- Généré le :  mer. 23 jan. 2019 à 15:51
+-- Version du serveur :  10.3.12-MariaDB
+-- Version de PHP :  7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,10 +31,18 @@ SET time_zone = "+00:00";
 CREATE TABLE `capteurs` (
   `id_capteur` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `nom` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `type` enum('HUM','FUM','TEMP','') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `etat` enum('ON','OFF','','') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` enum('HUM','FUM','TEMP') CHARACTER SET utf8mb4 COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `etat` enum('ON','OFF') COLLATE utf8mb4_unicode_ci NOT NULL,
   `id_piece` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `capteurs`
+--
+
+INSERT INTO `capteurs` (`id_capteur`, `nom`, `type`, `etat`, `id_piece`) VALUES
+('123456543456', 'test', 'TEMP', 'ON', 'c4df37ca-c8fc-4c9e-a4b6-5675a474c6c9'),
+('ewrtyjuiklouy', 'test', 'HUM', 'ON', 'cfbc0975-aff8-4ff8-b815-6bcdbfa3d911');
 
 -- --------------------------------------------------------
 
@@ -45,10 +53,17 @@ CREATE TABLE `capteurs` (
 CREATE TABLE `controleurs` (
   `id_controleur` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `nom` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `type` enum('LUM','CH','VOL','') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` enum('LUM','CH','VOL') COLLATE utf8mb4_unicode_ci NOT NULL,
   `etat` int(11) NOT NULL,
   `id_piece` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `controleurs`
+--
+
+INSERT INTO `controleurs` (`id_controleur`, `nom`, `type`, `etat`, `id_piece`) VALUES
+('efrtyujiko8lkujyh', 'test', 'VOL', 0, 'cfbc0975-aff8-4ff8-b815-6bcdbfa3d911');
 
 -- --------------------------------------------------------
 
@@ -62,6 +77,14 @@ CREATE TABLE `domiciles` (
   `cle_utilisateur` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Déchargement des données de la table `domiciles`
+--
+
+INSERT INTO `domiciles` (`id_domicile`, `nom`, `cle_utilisateur`) VALUES
+('4e1e30c3-f3b7-4b67-8fca-7debe6763d8d', 'Maison1', 'kjehkyehjvhguiytkdjg'),
+('a8e739de-c75a-4a88-bbb0-aa462a9923da', 'Maison2', 'asdfghtyjuikjytrfed');
+
 -- --------------------------------------------------------
 
 --
@@ -74,6 +97,15 @@ CREATE TABLE `pieces` (
   `id_domicile` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Déchargement des données de la table `pieces`
+--
+
+INSERT INTO `pieces` (`id_piece`, `nom`, `id_domicile`) VALUES
+('c1e35a48-c695-480e-89af-048e8c99e983', 'Cave', '4e1e30c3-f3b7-4b67-8fca-7debe6763d8d'),
+('c4df37ca-c8fc-4c9e-a4b6-5675a474c6c9', 'Salon', '4e1e30c3-f3b7-4b67-8fca-7debe6763d8d'),
+('cfbc0975-aff8-4ff8-b815-6bcdbfa3d911', 'sdb', 'a8e739de-c75a-4a88-bbb0-aa462a9923da');
+
 -- --------------------------------------------------------
 
 --
@@ -84,9 +116,18 @@ CREATE TABLE `releve_capteurs` (
   `id_releve` varchar(250) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `id_capteur` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `heure` datetime NOT NULL,
-  `type` enum('HUM','TEMP','','') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` enum('HUM','TEMP','FUM') COLLATE utf8mb4_unicode_ci NOT NULL,
   `donnee` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `releve_capteurs`
+--
+
+INSERT INTO `releve_capteurs` (`id_releve`, `id_capteur`, `heure`, `type`, `donnee`) VALUES
+('568e4a04-01f1-496e-80e9-ff05d7b848b4', '123456543456', '2019-01-23 14:44:05', 'TEMP', 29),
+('ee51b319-86d4-42f7-8645-668ef9a109c5', '123456543456', '2019-01-23 15:44:05', 'TEMP', 23),
+('f8656364-678d-4abc-9dc1-356b8bd7be26', 'ewrtyjuiklouy', '2019-01-23 15:44:05', 'HUM', 19);
 
 -- --------------------------------------------------------
 
@@ -98,9 +139,17 @@ CREATE TABLE `releve_controleurs` (
   `id_releve` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `id_controleur` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `heure` datetime NOT NULL,
-  `type` enum('LUM') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` enum('LUM','CH','VOL') COLLATE utf8mb4_unicode_ci NOT NULL,
   `donnee` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `releve_controleurs`
+--
+
+INSERT INTO `releve_controleurs` (`id_releve`, `id_controleur`, `heure`, `type`, `donnee`) VALUES
+('94aa2a49-8d8d-4236-be7b-40bda274fb06', 'efrtyujiko8lkujyh', '2019-01-23 15:44:05', 'VOL', 0),
+('eaad044f-4973-4dcf-9638-5b4c6331c1fc', 'efrtyujiko8lkujyh', '2019-01-23 14:44:05', 'VOL', 0);
 
 -- --------------------------------------------------------
 
@@ -122,6 +171,14 @@ CREATE TABLE `utilisateurs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`cle`, `nom`, `prenom`, `email`, `mdp`, `adresse`, `ville`, `pays`, `telephone`, `admin`) VALUES
+('asdfghtyjuikjytrfed', 'Michel', 'Michel', 'leoantoineberton@yahoo.com', '$2y$10$1HglNhK16qGQM/SrTMp9MOO.2FpHGmCQcIf6aKwhC2B6FL./92qwq', '14 bis rue de james', 'Paris', 'france', '0651458429', 0),
+('kjehkyehjvhguiytkdjg', 'Berton', 'Leo', 'leoantoineberton@gmail.com', '$2y$10$LXBaI1aqWTNylGa/g2h6luDYv7NLDjeKhlhrE6whZp/sHUi8lQ1gG', '46 rue des lombards', 'Paris', 'france', '0651458429', 0);
+
+--
 -- Index pour les tables déchargées
 --
 
@@ -130,27 +187,28 @@ CREATE TABLE `utilisateurs` (
 --
 ALTER TABLE `capteurs`
   ADD PRIMARY KEY (`id_capteur`),
-  ADD KEY `capteur_ibfk_1` (`id_piece`);
+  ADD KEY `id_piece` (`id_piece`);
 
 --
 -- Index pour la table `controleurs`
 --
 ALTER TABLE `controleurs`
   ADD PRIMARY KEY (`id_controleur`),
-  ADD KEY `controleur_ibfk_1` (`id_piece`);
+  ADD KEY `id_piece` (`id_piece`);
 
 --
 -- Index pour la table `domiciles`
 --
 ALTER TABLE `domiciles`
-  ADD PRIMARY KEY (`id_domicile`);
+  ADD PRIMARY KEY (`id_domicile`),
+  ADD KEY `cle` (`cle_utilisateur`);
 
 --
 -- Index pour la table `pieces`
 --
 ALTER TABLE `pieces`
   ADD PRIMARY KEY (`id_piece`),
-  ADD KEY `piece_ibfk_1` (`id_domicile`);
+  ADD KEY `id_domicile` (`id_domicile`);
 
 --
 -- Index pour la table `releve_capteurs`
@@ -180,31 +238,37 @@ ALTER TABLE `utilisateurs`
 -- Contraintes pour la table `capteurs`
 --
 ALTER TABLE `capteurs`
-  ADD CONSTRAINT `capteur_ibfk_1` FOREIGN KEY (`id_piece`) REFERENCES `pieces` (`id_piece`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `capteurs_ibfk_1` FOREIGN KEY (`id_piece`) REFERENCES `pieces` (`id_piece`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `controleurs`
 --
 ALTER TABLE `controleurs`
-  ADD CONSTRAINT `controleur_ibfk_1` FOREIGN KEY (`id_piece`) REFERENCES `pieces` (`id_piece`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `controleurs_ibfk_1` FOREIGN KEY (`id_piece`) REFERENCES `pieces` (`id_piece`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `domiciles`
+--
+ALTER TABLE `domiciles`
+  ADD CONSTRAINT `domiciles_ibfk_1` FOREIGN KEY (`cle_utilisateur`) REFERENCES `utilisateurs` (`cle`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `pieces`
 --
 ALTER TABLE `pieces`
-  ADD CONSTRAINT `piece_ibfk_1` FOREIGN KEY (`id_domicile`) REFERENCES `domiciles` (`id_domicile`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `pieces_ibfk_1` FOREIGN KEY (`id_domicile`) REFERENCES `domiciles` (`id_domicile`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `releve_capteurs`
 --
 ALTER TABLE `releve_capteurs`
-  ADD CONSTRAINT `id_capteur` FOREIGN KEY (`id_capteur`) REFERENCES `capteurs` (`id_capteur`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `releve_capteurs_ibfk_1` FOREIGN KEY (`id_capteur`) REFERENCES `capteurs` (`id_capteur`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `releve_controleurs`
 --
 ALTER TABLE `releve_controleurs`
-  ADD CONSTRAINT `id_controleur` FOREIGN KEY (`id_controleur`) REFERENCES `controleurs` (`id_controleur`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `releve_controleurs_ibfk_1` FOREIGN KEY (`id_controleur`) REFERENCES `controleurs` (`id_controleur`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
